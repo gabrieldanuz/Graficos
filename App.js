1283ecfa84f94552b203fc801a63e01df6101996
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2021-11-12 23:34:40 -0300

    Graficos

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-svg'
+import { PieChart } from 'react-native-svg-charts'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const data = [30, 10, 25, 18, 17]
+  const pieData = data.map((value, index) => ({
+    value,
+    key: `${index}-{value}`,
+    svg: {
+      fill: (
+        '#' +
+        ((Math.random() * 0xffffff) << 0).toString(16) +
+        '000000'
+      ).slice(0, 7)
+    }
+  }))
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const Label = ({ slices }) => {
+    return slices.map((slice, index) => {
+      const { pieCentroid, data } = slice
+      return (
+        <Text
+          key={`label-${index}`}
+          x={pieCentroid[0]}
+          y={pieCentroid[1]}
+          fill="black"
+          textAnchor={'middle'}
+          alignmentBaseline={'middle'}
+          fontSize={22}
+        >
+          {data.value}
+        </Text>
+      )
+    })
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <PieChart style={{ height: 400 }} data={pieData}>
+        <Label />
+      </PieChart>
+    </View>
+  )
+}
